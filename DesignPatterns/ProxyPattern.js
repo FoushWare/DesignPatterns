2f7144d16d0b549c385ee47a1ca49c54569c900e
FoushWare <Ahmed.fouad@kemetai.com>
2022-06-04 16:16:12 +0200

    Proxy Design pattern add validation before set

--- DesignPatterns/ProxyPattern.js
+++ DesignPatterns/ProxyPattern.js
@@ -1,44 +1,32 @@
-/** @format */
 
-//proxy pattern
 const person = {
-	name: "John",
-	age: 30,
-	job: "developer",
-	sayHello: function () {
-		console.log(`Hello, my name is ${this.name}`);
-	},
+	name: "John Doe",
+	age: 42,
+	nationality: "American"
 };
 
-// proxy object
-// const personProxy = new Proxy(person, {
-// 	get: function (obj, prop) {
-// 		if (prop in obj) {
-// 			return obj[prop];
-// 		} else {
-// 			console.log(`Property ${prop} does not exist`);
-// 		}
-// 	},
-// 	set: function (obj, prop, value) {
-// 		if (prop in obj) {
-// 			obj[prop] = value;
-// 			return true
-// 		} else {
-// 			console.log(`Property ${prop} does not exist`);
-// 		}
-// 	},
-// });
 const personProxy = new Proxy(person, {
-	get: function (obj, prop) {
-		console.log(`The value of ${prop} is ${obj[prop]}`);
+	get: (obj, prop) => {
+		if (!obj[prop]) {
+			console.log(`Hmm.. this property doesn't seem to exist`);
+		} else {
+			console.log(`The value of ${prop} is ${obj[prop]}`);
+		}
 	},
-	set: function (obj, prop, value) {
-		console.log(`Changed ${prop} from ${obj[prop]} to ${value}`);
-		obj[prop] = value;
+	set: (obj, prop, value) => {
+		if (prop === "age" && typeof value !== "number") {
+			console.log(`Sorry, you can only pass numeric values for age.`);
+		} else if (prop === "name" && value.length < 2) {
+			console.log(`You need to provide a valid name.`);
+		} else {
+			console.log(`Changed ${prop} from ${obj[prop]} to ${value}.`);
+			obj[prop] = value;
+		}
 		return true;
-	},
+	}
 });
 
+personProxy.nonExistentProperty;
+personProxy.age = "44";
+personProxy.name = "";
 
-personProxy.name;
-personProxy.age = 10;
